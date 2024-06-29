c21ed3e37d82aea221e1e804020b4214d0e90333
kararei <kararei@gmail.com>
2024-06-30 01:47:12 +0800

    Add components and pw reset handler

--- app/(auth)/forget-pw.jsx
+++ app/(auth)/forget-pw.jsx
@@ -7,3 +7,30 @@ import { FontAwesome } from '@expo/vector-icons';
 import { sendPasswordResetEmail } from 'firebase/auth';
 import { auth } from '../firebase';
 
+export default function ForgotPassword() {
+    const [fontsLoaded] = useFonts({
+      'Poppins-SemiBold': require('../../assets/fonts/Poppins-SemiBold.ttf'),
+      'Poppins-Regular': require('../../assets/fonts/Poppins-Regular.ttf'),
+    });
+  
+    const [email, setEmail] = useState('');
+
+    if (!fontsLoaded) {
+        return null; // or a loading spinner
+      }
+
+      const handlePasswordReset = async () => {
+        if (!email) {
+          Alert.alert('Please enter your email to reset password');
+          return;
+        }
+    
+        try {
+          await sendPasswordResetEmail(auth, email);
+          Alert.alert('Password reset email sent');
+        } catch (error) {
+          const errorMessage = error.message;
+          Alert.alert('Error:', errorMessage);
+        }
+      };
+    
